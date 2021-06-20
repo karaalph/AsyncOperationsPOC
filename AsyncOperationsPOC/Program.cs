571c9b8096e262a095c0746cb68863af3eeed903
huseyin.karaalp <huseyin.karaalp@trendyol.com>
2021-06-20 21:59:46 +0300

    test

--- AsyncOperationsPOC/Program.cs
+++ AsyncOperationsPOC/Program.cs
@@ -6,7 +6,7 @@ namespace AsyncOperationsPOC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ekip!");
         }
     }
 }
