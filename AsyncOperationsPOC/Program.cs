74eeaa5cdf417797b036f1dba423f603502e3f45
elif.bayrakdar <elif.bayrakdar@trendyol.com>
2021-06-20 21:44:03 +0300

    Initial Commit

---
+++ AsyncOperationsPOC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncOperationsPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
