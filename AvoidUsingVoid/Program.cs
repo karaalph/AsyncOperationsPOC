d57cb9ce973b09c6813a417d1b233a9a8f0f1da1
huseyin.karaalp <huseyin.karaalp@trendyol.com>
2021-06-23 00:32:21 +0300

    Add avoid using void

---
+++ AvoidUsingVoid/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AvoidUsingVoid
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("start");
+            
+            
+            /* --1--
+             * ThrowException();
+             */
+            
+            /* --2--
+            try
+            {
+                 VoidFuncAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            */
+            
+            /* --3--
+            try
+            {
+                 await TaskFuncAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            */
+            Console.WriteLine("end");
+            Console.ReadKey();
+        }
+        
+        public static async Task ThrowException()
+        {
+            throw new Exception();
+        }
+
+        public static async void VoidFuncAsync()
+        {
+            await ThrowException();
+        }
+
+        public static async Task TaskFuncAsync()
+        {
+            await ThrowException();
+        }
+    }
+}
